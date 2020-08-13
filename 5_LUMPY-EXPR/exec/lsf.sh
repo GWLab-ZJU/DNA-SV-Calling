c4d7bd72af148f3f7265316d5a3887644d2e1bc7
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-08-14 00:50:43 +0800

    Small bugs fixed

--- 5_LUMPY-EXPR/exec/lsf.sh
+++ 5_LUMPY-EXPR/exec/lsf.sh
@@ -2,11 +2,11 @@ tdm="../4_BWA_GATK/${line}_dupmark.bam"
 tdc="${line}_LUMPY_discordants.bam"
 tds="${line}_LUMPY_splitters.bam"
 [ -f "${tdm}" ]
-if [ ! -f "${tdc}" ] || [ ! -f "${tds}" ];then
-    LIBDO_LOG_MODE=2
-    DO samtools view -b -F 1294 "${tdm}" \| samtools sort -o "${tdc}" -
-    DO samtools view -h "${tdm}" \| "${LUMPYscripts}"/extractSplitReads_BwaMem -i stdin \| samtools view -Sh -\|samtools sort -o "${tds}" -
-    LIBDO_LOG_MODE=4
-fi
+
+LIBDO_LOG_MODE=2
+[ ! -f "${tdc}" ] && DO samtools view -b -F 1294 "${tdm}" \| samtools sort -o "${tdc}" - || true
+[ ! -f "${tds}" ] && DO samtools view -h "${tdm}" \| "${LUMPYscripts}"/extractSplitReads_BwaMem -i stdin \| samtools view -Sh -\|samtools sort -o "${tds}" - || true
+LIBDO_LOG_MODE=4
+
 DO lumpyexpress -B "${tdm}" -S "${tds}" -D "${tdc}" -o "${line}"_raw.vcf
 DO svtyper -i "${line}"_raw.vcf -B "${tdm}" -o VCF/"${line}"_LUMPY-EXPR.vcf
