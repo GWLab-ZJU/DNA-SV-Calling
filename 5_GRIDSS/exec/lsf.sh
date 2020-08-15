bb5ca64fdb87ab83cf651170fbae65ce9660f4c1
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-08-15 22:50:11 +0800

    Small bugs fixed

--- 5_GRIDSS/exec/lsf.sh
+++ 5_GRIDSS/exec/lsf.sh
@@ -1,3 +1,3 @@
 tumour="../4_BWA_GATK/${line}_dupmark.bam"
 [ -f "${tumour}" ]
-DO gridss.sh --reference "${ref}" --output VCF/"${line}"_gridss.vcf --assembly "${line}"_assembly.bam --jar "${GRIDSS_JAR}" --steps All --labels "${line}" "${tumour}"
+DO gridss.sh --reference "${bwaref}" --output VCF/"${line}"_gridss.vcf --assembly "${line}"_assembly.bam --jar "${GRIDSS_JAR}" --steps preprocess,assemble,call --labels "${line}" "${tumour}"
