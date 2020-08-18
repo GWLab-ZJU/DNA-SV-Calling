eecf3646e3715264d257225c8f8f900220c15aab
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-08-18 15:55:03 +0800

    Small bugs fixed

--- 5_DIGIT/exec/lsf.sh
+++ 5_DIGIT/exec/lsf.sh
@@ -6,6 +6,6 @@ if ! [ -f "${tumour}" ];then
 	DO samtools view -F 256 -F 2048 "${line}"_nmsorted.bam -o "${tumour}"
 fi
 
-DO digit analyse -i "${PWD}/${tumour}" -o "${PWD}/${line}" -s 2.33 -a false -lc "${lcbed}" -r "${HG}"/Homo_sapiens_assembly38.idx.ChrLen.txt -q 0
+DO digit analyse -i "${PWD}/${tumour}" -o "${PWD}/${line}" -s 2.33 -a false -lc "${lcbed}" -r "${ref}".idx.ChrLen.txt -q 0
 
-DO digit proxval -T 100 -c 3 -i "${PWD}/${line}_DIGIT_FINAL_trl.sam" -o "${PWD}/${line}_trl_clusters" -g "${HG}"/Homo_sapiens_assembly38.fasta -x "${HG}"/Homo_sapiens_assembly38.idx -M -1 -t file=m"${line}"_summary.txt -r "${HG}"/Homo_sapiens_assembly38.idx.ChrLen.txt
+DO digit proxval -T 100 -c 3 -i "${PWD}/${line}_DIGIT_FINAL_trl.sam" -o "${PWD}/${line}_trl_clusters" -g "${ref}" -x "${ref}".idx -M -1 -t file=m"${line}"_summary.txt -r "${ref}".idx.ChrLen.txt
